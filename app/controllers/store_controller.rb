b89bc6aa0ea34f5e23f79017aa5165156d457a40
soragu <soragu@163.com>
2015-01-27 16:03:13 +0800

    Before Use Ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.all.order(:title)
+    #@cart = current_cart
   end
 end
