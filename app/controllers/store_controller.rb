951c107a30c6fa0a4f966cf6e56a80c9428cb6df
soragu <soragu@163.com>
2015-01-27 12:23:05 +0800

    Add to Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,5 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
-  default_scope {order('title')}
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title ,:description ,:image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true, :length => {:minimum => 10}
@@ -7,4 +10,19 @@ class Product < ActiveRecord::Base
                           :with => %r{\.(gif|jpg|png)\Z}i,
                           :message =>  'must be a url for gif jpg or png'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
